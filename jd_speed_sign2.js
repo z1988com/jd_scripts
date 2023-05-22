4b41813f9a0fe69046ba179d07eab00cab38c19b
z1988 <z1988com@qq.com>
2023-05-22 14:58:36 +0800

    0

--- jd_speed_sign2.js
+++ jd_speed_sign2.js
@@ -40,7 +40,7 @@ if (new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate() == date.getDa
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', { "open-url": "https://bean.m.jd.com/bean/signIndex.action" });
     return;
   }
-  for (let i = 0; i < cookiesArr.length; i++) {
+  for (let i = 1; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
       var cookie = cookiesArr[i];
 	    var url_uuid = randomString(16);
@@ -85,9 +85,9 @@ async function jdGlobal(cookie,url_uuid) {
 	if (!isLogin)
 		return;
 	
-    await wheelsHome(cookie)
+    //await wheelsHome(cookie)
     await apTaskList(cookie)
-    await wheelsHome(cookie)
+    //await wheelsHome(cookie)
     await taskList(cookie,url_uuid)
     if (llAPIError) {      
       return;
